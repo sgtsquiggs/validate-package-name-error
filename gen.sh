21e697baf0f8e6e99c1f08e126f95ae3a2a5ce26
Matthew Crenshaw <mcrenshaw@clearstreet.io>
2021-08-09 16:32:03 -0400

    add package map opts to validate_out

--- gen.sh
+++ gen.sh
@@ -6,10 +6,12 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26.0
 rm -rf gen/go
 mkdir -p gen/go
 
+PKGMAP="Mtestsvc/person/v1/person.proto=github.com/sgtsquiggs/validate-package-name-error/gen/go/testsvc/person/v1/person;testsvc_person_v1,Mtestsvc/test/v1/enums.proto=github.com/sgtsquiggs/validate-package-name-error/gen/go/testsvc/test/v1/person;testsvc_test_v1"
+
 protoc \
   -I proto \
   -I "${GOPATH}/pkg/mod/github.com/envoyproxy/protoc-gen-validate@v0.5.1" \
-  --go_out="paths=source_relative,Mtestsvc/person/v1/person.proto=github.com/sgtsquiggs/validate-package-name-error/gen/go/testsvc/person/v1/person;testsvc_person_v1,Mtestsvc/test/v1/enums.proto=github.com/sgtsquiggs/validate-package-name-error/gen/go/testsvc/test/v1/person;testsvc_test_v1:gen/go" \
-  --validate_out="lang=go,paths=source_relative:gen/go" \
+  --go_out="paths=source_relative,${PKGMAP}:gen/go" \
+  --validate_out="lang=go,paths=source_relative,${PKGMAP}:gen/go" \
   testsvc/person/v1/person.proto testsvc/test/v1/enums.proto
 
